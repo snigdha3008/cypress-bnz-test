e668bfadd9a009cb2b546a1433bdc07508962f26
snigdha3008 <snigdha3008@gmail.com>
2023-03-09 22:13:45 +1300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   projectId: "ht3p9v",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      
+    defaultCommandTimeout:15_000
     },
   },
 };
